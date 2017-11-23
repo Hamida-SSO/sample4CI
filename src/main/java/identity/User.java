8cb273e0f5b8196fd5ac96bc4cbab7373f58af45
Hamida SIDI SAID OMAR <hsidisaidomar@DIGINAMIC.LOCAL>
2017-11-23 15:10:47 +0100

    Corréction error

--- src/main/java/identity/User.java
+++ src/main/java/identity/User.java
@@ -14,14 +14,14 @@ public class User {
     public void setLastName(String lastName) {
         this.lastName = "lastName=" + lastName;
     }
-/*
+
     public Long getId() {
 		return id;
 	}
 
 	public void setId(Long id) {
 		this.id = id;
-	}*/
+	}
 
 	public String getFirstName() {
         return firstName;
@@ -30,6 +30,16 @@ public class User {
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
 
     @Override
     public boolean equals(Object o) {
@@ -44,7 +54,7 @@ public class User {
     @Override
     public String toString() {
         if (id == null) {
-            return null;
+            return "";
         }
         return "id=" + id + ", lastName=" + lastName + ", firstName=" + firstName;
     }
